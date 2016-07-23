115540593676a14e3f92cffff524de4bf1beb223
Sriram Pulipaka <srk@sriram.pulipaka>
2016-07-22 20:19:45 -0400

    Initial Push

---
+++ scripts/TestScript.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by srk on 7/12/16.
+  */
+
+println("In Test Script")
+
+val list = List(1, 2, 3, 4)
+println("List size: " + list.size)
---
+++ src/main/scala/TestObject.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by srk on 7/12/16.
+  */
+object TestObject {
+  def main(args: Array[String]): Unit = {
+    println("In main method, in an object")
+  }
+}
