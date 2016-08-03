e3a350b4fc705de47b69e2ebc14b443d91e88f3c
Sriram Pulipaka <sriram.pulipaka@dealertrack.com>
2016-08-03 00:01:38 -0400

    Updated TestScript with List methods

--- scripts/TestScript.scala
+++ scripts/TestScript.scala
@@ -1,4 +1,23 @@
-println("In Test Script")
+import scala.sys.process._
 
-val list = List(1, 2, 3, 4)
-println("List size: " + list.size)
+"clear".!
+println("\n///////////////////////////////////////////////\n")
+
+println(Nil.::(10).::(11)) // prints List(11, 10)
+println(10 :: 11 :: Nil) // prints List(10, 11)
+
+println("\n///////////////////////////////////////////////\n")
+
+val list1 = List("test1", "test2", "test3")
+list1.foreach(println)
+println("list1 -> " + list1)
+println("list1.head -> " + list1.head)
+println("list1.tail -> " + list1.tail)
+println("list1.last -> " + list1.last)
+println("list1.map -> " + list1.map(string => string + "_appeneded"))
+println("list1.mkString -> " + list1.mkString(", "))
+println("list1 -> " + list1)
+
+println("\n///////////////////////////////////////////////\n")
+
+
